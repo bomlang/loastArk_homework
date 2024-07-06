9e87eb50de572c09e057d1e7c38bd83a132569a3
bomlang <bomlang4211@gmail.com>
2024-07-06 16:52:19 +0900

    chore: add and setting @tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import Router from './pages/Router'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Toaster />
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Toaster />
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 )
